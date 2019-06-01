8eb2480df90c7d474bff4e7d50cc509588c6536e
Sitansu Subudhi <silu.subudhi@gmail.com>
2019-06-01 19:22:04 +0530

    Decimal format print

--- Introduction/printformat.c
+++ Introduction/printformat.c
@@ -3,5 +3,5 @@
 int main() {
 	printf("%d\n", 10);
 	printf("%6d\n", 10);
-	printf("%6.2f\n",4.89);
+	printf("%6.2f\n",4.89);		//prints 2 decimal places and gives total 6 spaces including decimal value
 }
