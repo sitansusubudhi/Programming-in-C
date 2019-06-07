89bd5cf7d721a156d34a75c7116e550d59bedfae
Sitansu Subudhi <silu.subudhi@gmail.com>
2019-06-07 08:31:16 +0530

    resizing using realloc for dynamically allocated elements using calloc

--- Storage-Classes/mcrf.c
+++ Storage-Classes/mcrf.c
@@ -3,18 +3,22 @@
 
 int main() {
 	int *mall, *call, *reall;
-	int no_of_elements = 5;
+	int no_of_elements = 3;
 	mall = (int*)malloc(sizeof(int));
 	call = (int*)calloc(no_of_elements, sizeof(int)); //Initialises to 0 all values
-	reall = (int*)realloc(call,sizeof(int));
+	printf("\nCalloc address before realloc %U", call);
+	printf("\nCalloc value %d", *call);
+	//realloc(call,900*sizeof(int));
+	reall = (int*)realloc(call,90*sizeof(int));
 	*mall = 50;
-	printf("\n%U", mall);
-	printf("\n%d", *mall);
-	printf("\n%U", call);
-	printf("\n%U", reall);
-	printf("\n%d", *reall);
+	printf("\nMalloc address %U", mall);
+	printf("\nMalloc value %d", *mall);
+	printf("\nCalloc address %U", call); 
+	printf("\nCalloc value %d", *call); //garbage value at location pointed by call variable
+	printf("\nRealloc address %U", reall);
+	printf("\nRealloc value %d", *reall); // all original contents are copied into new location hence value 0
 	free(mall);
-	//free(call);
+	//free(call); //no longer available
 	free(reall);
 	return 0;
 
