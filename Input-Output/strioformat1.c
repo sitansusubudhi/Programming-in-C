430c4c3be0a2d656925bcca37d457a97b6dab600
Sitansu Subudhi <silu.subudhi@gmail.com>
2019-06-08 23:43:28 +0530

    %w.ns format specifier for printf

---
+++ Input-Output/strioformat1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	char str[10];
+	scanf("%1s", str);
+	printf("\n%s", str);
+	printf("\n%4s", "ravindra");
+	printf("\n%.4s", "ravindra");
+	printf("\n%8.4s", "ravindra");
+	return 0;
+}
