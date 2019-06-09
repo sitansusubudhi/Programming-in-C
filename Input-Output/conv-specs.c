0705471ce4895ff733c11e71676ce3da6e076da7
Sitansu Subudhi <silu.subudhi@gmail.com>
2019-06-09 08:15:23 +0530

    Conversion specifications %x %d %u %c %i

---
+++ Input-Output/conv-specs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	unsigned char a = 255;
+	int num = 4294967295;
+	printf("\n%u",a);
+	printf("\n%d",sizeof(int));
+	printf("\n%c",a);
+	printf("\n%d",a);
+
+	printf("\n%u",num);
+	printf("\n%d",num);
+	return 0;
+}
---
+++ Input-Output/signeddecimalhexa1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+	int a;
+	scanf("%i", &a);
+	printf("decimal a = %d\n", a);
+	printf("hexa decimal a = %x\n", a);
+	return 0;
+}
